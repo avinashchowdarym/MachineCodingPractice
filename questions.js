b851ae733cd6060c46dcfc10bbc2d80dbef5f1ba
mavinashchowdary <mavinashchowdaryofficial@gmail.com>
2025-03-12 08:19:54 +0530

    added a few

---
+++ questions.js
@@ -0,0 +1,19 @@
+// two sum 
+
+const arr = [1,2,3,4,5,6]; // )(logn)
+
+function twosum(arr , target){
+    let resarr = [] 
+    const mapped = new Set(); // O(1) 
+    for(let i=0;i<arr.length;i++){
+        if(mapped.has(target - arr[i])){
+            resarr.push([arr[i],target-arr[i]]);
+        }
+        mapped.add(arr[i]); //O(1)
+    }
+    return resarr; // O(n)
+}
+
+console.log(twosum(arr,7));
+
+// 1-2-3 
