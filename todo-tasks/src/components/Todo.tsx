d0deffefde432d1544fcc3b5b0cf2797f7f5da99
mavinashchowdary <mandalapuavinash.chowdary@gmail.com>
2025-03-14 23:51:13 +0530

    basics done , need to add few more functionalities before backend

--- todo-tasks/src/components/Todo.tsx
+++ todo-tasks/src/components/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { ReactElement, useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { Checkbox } from '@radix-ui/react-checkbox'
@@ -11,44 +11,78 @@ type Todo = {
 }
 
 const Todo = () => {
-    const [task,setTask] = useState<string>('');
+    const [task, setTask] = useState<string>('');
     // const [error,setError] = useState<string|null>('');
-    const [todos,setTodos] = useState<Todo[]>([])
+    const [todos, setTodos] = useState<Todo[]>([])
+    const [completed, setCompleted] = useState<Todo[]>([]);
 
     const handleClick = () => {
-        // if(!task.trim()){
-            
-        // }
-        
         setTodos(
-            [{id: Date.now() , task: task , isDone: false},...todos]
+            [{ id: Date.now(), task: task, isDone: false }, ...todos]
         )
         setTask('');
+
     }
-   
-  return (
-    <div className='flex flex-col mt-20'>
-        <div className='flex flex-row gap-8'>
-            <Input placeholder='add tasks' value={task} onChange={(e) => setTask(e.target.value)}/>
-            <Button variant='outline' onClick={handleClick}>Add Todo</Button>
-        </div>
-    { todos.length !== 0 && <div className=' m-20 p-10 w-[50vw]'>
-            {
-                todos?.map((todo)=>{
-                    return(
-                        <div className = 'border-1 border-gray-450 p-3 m-4' key={todo.id}>
-                            <div>
-                                <Checkbox/>
-                                <span>{todo.task}</span>
+
+    const handleCompleted = (id: Number) => {
+        const newArr = todos.filter((task) => task.id !== id);
+        setTodos(newArr);
+        const compeltedArr = todos.filter((task) => task.id === id);
+        setCompleted(prev => [...prev, ...compeltedArr]);
+    }
+
+
+    return (
+        <div className='flex flex-col mt-20 w-[50%]'>
+            <div className='flex flex-row gap-8'>
+                <Input placeholder='add tasks' onKeyDown={(e)=> e.key === 'Enter' ? handleClick() : ''}  value={task} onChange={(e) => setTask(e.target.value)} />
+                <Button variant='outline' onClick={handleClick} disabled={task.trim().length === 0}>Add Todo</Button>
+                <Checkbox />
+            </div>
+            <div className='text-2xl mt-20'>Tasks</div>
+
+            {todos.length !== 0 ? <div className=' mt-10 p-10 w-[50vw]'>
+                {
+                    todos?.map((todo) => {
+                        return (
+                            <div className="items-top flex justify-between items-center space-x-2 border-1 p-3 mt-3 border-gray-450" key={todo.id}>
+                                {/* <input className='dark:bg-black border-white' type='checkbox' checked={todo.isDone} onChange={(e)=>handleCheckClick(e)}/> */}
+                                <div className="grid gap-1.5 leading-none">
+                                    <label
+                                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                    >
+                                        {todo.task}
+                                    </label>
+                                </div>
+                                <Button className='p-2' variant='outline' size='icon' onClick={() => handleCompleted(todo.id)}>D</Button>
+                            </div>
+                        )
+                    })
+                }
+
+            </div> : <div className='text-blue-200 m-auto mt-20'>Empty Tasks</div>}
+            {completed.length !== 0 && <div className='mt-20 text-2xl'>Completed Tasks</div>
+            }                {
+                completed?.map((todo) => {
+                    return (
+                        <div className="items-top flex space-x-2 border-1 p-3 m-3 border-gray-450 rounded-xl" key={todo.id}>
+                            {/* <Checkbox checked={todo.isDone} className='flex' /> */}
+                            {/* <input className='dark:bg-black border-white' type='checkbox' checked={todo.isDone} onChange={(e)=>handleCheckClick(e)}/> */}
+                            <div className="grid gap-1.5 leading-none">
+
+                                <label
+                                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                >
+                                    <s>{todo.task}</s>
+                                </label>
                             </div>
                         </div>
                     )
                 })
             }
-        </div>}
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Todo
